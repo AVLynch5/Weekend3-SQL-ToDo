c487a3d3ddbbe5e18e5d5cc9c61cf556f8493a2f
TJ Lynch <anthonyvlynch5@gmail.com>
2021-09-10 01:51:37 -0400

    todo.router - created router.get method call

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -5,6 +5,15 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 //GET
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "todo" ORDER BY "id" LIMIT 50;';
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('error during GET', error);
+        res.sendStatus(500);
+    });
+});
 
 //POST
 
