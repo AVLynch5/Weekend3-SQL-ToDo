79db5ea52f61efa69bef5c2e5b464f51ea41cd5e
TJ Lynch <anthonyvlynch5@gmail.com>
2021-09-09 21:56:30 -0400

    updated todo.router.js with express and router setup

---
+++ server/routes/todo.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+//input pool
+const pool = require('../modules/pool');
+
+//GET
+
+//POST
+
+//PUT
+
+//DELETE
+
+module.exports = router;
